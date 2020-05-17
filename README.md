550af69b5d349dd467c04761f68bd0298141aa41
Daniel Winterstein <daniel@good-loop.com>
2020-05-17 11:59:03 +0100

    npm cli

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# java-bob
+# Welcome to Bob-the-Builder, the Java-friendly code builder & deployment tool
+
+This package `java-bob` provides an npm wrapper for using Bob.
+
+Install with 
+
+`npm -g i java-bob`
+
+Run by `bob` from the project folder.
+
+Use `bob --help` for help with options.
--- bin/bob.js
+++ bin/bob.js
@@ -3,6 +3,9 @@ const shell = require("shelljs");
 const https = require('https');
 const fs = require('fs');
 
+const jar = 'bob-all.jar';
+const cmd = 'bob';
+
 //console.log(process.argv);
 const args = [...process.argv];
 // shidt node command, script path
@@ -10,20 +13,23 @@ args.shift();
 const dirPath = args.shift();
 console.log(dirPath, args);
 
-const jarExists = fs.existsSync('bob-all.jar');
+const jarExists = fs.existsSync(jar);
 
 if ( ! jarExists || args[0]==="-update" || args[0]==="--update") {
-	console.log("Downloading latest bob-all.jar...");	
-	const file = fs.createWriteStream("bob-all.jar");
-	const request = https.get("https://www.winterwell.com/software/downloads/bob-all.jar", function(response) {
+	console.log("Downloading latest "+jar+"...");
+	// cross-platform download -- sadly not as 100% reliable as wget
+	const file = fs.createWriteStream(jar);
+	const request = https.get("https://www.winterwell.com/software/downloads/"+jar, function(response) {
 		  response.pipe(file);
 		  console.log("...Download complete.");	
+		  if (args[0]==="-update" || args[0]==="--update") {
+			return; // dont go on to build a site
+		}
+		console.log("Note: If you get a message 'Invalid or corrupt jarfile' - Try re-running "+cmd);
+		console.log("If it repeats, run `"+cmd+" -update` to download the jar again.");	
 	});
-	if (args[0]==="-update" || args[0]==="--update") {
-		return; // dont go on to build a site
-	}
-	console.log("Note: If you get a message 'Invalid or corrupt jarfile' - Try re-running bob.");
+	return;
 }
 
-shell.exec("java -jar bob-all.jar "+args.join(" "));
+shell.exec("java -jar "+jar" "+args.join(" "));
 
